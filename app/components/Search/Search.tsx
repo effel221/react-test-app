fe4f74ac6d4b2d90ad650faf18c3acded3d413ad
Tatsiana-Salikayeva <tatsiana.salikayeva@promaterial.com>
2024-03-02 22:54:25 +0100

    remove not needed code

--- app/components/Search/Search.tsx
+++ app/components/Search/Search.tsx
@@ -1,14 +1,8 @@
 // import styles
 import styles from "./Search.module.scss";
-import {useAppDispatch, useAppStore} from "../../lib/hooks";
-import {useRef} from "react";
 
 
 export default function Search(): React.JSX.Element {
-    const store = useAppStore()
-    const initialized = useRef(false)
-
-    const dispatch = useAppDispatch()
   return (
       <div className={styles.searchBlock}>
           <input type="text" className={styles.searchBlockInput} placeholder={'Search packages...'}/>
