5f432cdb2150c58b4fd8f0cbac33f9f14af0b89b
Tatsiana-Salikayeva <tatsiana.salikayeva@promaterial.com>
2024-03-03 19:16:32 +0100

    clean up usePackageSearch hook

--- app/lib/usePackageSearch.tsx
+++ app/lib/usePackageSearch.tsx
@@ -4,29 +4,26 @@ import {SearchCardType} from "../components/BaseComponents/SearchCard/SearchCard
 const url = 'https://libraries.io/api/search'
 const githubKey = 'adea9f496220f5bf0a57a2fc91bfe40b'
 const searchCache = {};
-const getPackages = async (value: string, setIsLoading: (boolean)=>void) => {
-    setIsLoading(true)
-    if (!searchCache[value]) {
-      const response = await fetch(`${url}?q=${value}&api_key=${githubKey}`)
-      const packagesData = await response.json();
-      searchCache[value] = packagesData;
-    }
-    setIsLoading(false)
-    return searchCache[value];
+
+const getPackages = async (value: string) => {
+  const response = await fetch(`${url}?q=${value}&api_key=${githubKey}`)
+  const packagesData = await response.json();
+  searchCache[value] = packagesData;
 }
 
 export const usePackageSearch = (value: string, setIsLoading: (boolean)=>void): [SearchCardType] => {
-    const [packages, setPackages] = useState<[SearchCardType]>([])
-    const [isResultLoaded, setIsResultLoaded ] = useState<boolean>(false)
+    const [packages, setPackages] = useState<[SearchCardType]>([]);
+    const [isResultLoaded, setIsResultLoaded ] = useState<boolean>(false);
     useEffect(()=> {
-     setIsResultLoaded(false)
-     value.length && getPackages(value, setIsLoading).then(() => setIsResultLoaded(true))
-     !value.length && setPackages([])
-    }, [value]);
-
-    useEffect(()=> {
-        isResultLoaded && setPackages(searchCache[value] || [])
-    }, [isResultLoaded, value]);
-
+      setIsResultLoaded(false)
+      if (!value.length) return setPackages([])
+      if (searchCache[value]) {
+          setPackages(searchCache[value] || []);
+          setIsLoading(false)
+      } else {
+          setIsLoading(true)
+          getPackages(value).then(() => setIsResultLoaded(true))
+      }
+    }, [value, isResultLoaded]);
     return packages;
 };
