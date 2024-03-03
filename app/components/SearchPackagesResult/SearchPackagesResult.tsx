b67520db812aa8e10157feebbbd1da42fc2b595a
Tatsiana-Salikayeva <tatsiana.salikayeva@promaterial.com>
2024-03-03 17:52:53 +0100

    add loading while seach

--- app/components/SearchPackagesResult/SearchPackagesResult.tsx
+++ app/components/SearchPackagesResult/SearchPackagesResult.tsx
@@ -1,6 +1,6 @@
 "use client"
 //import  components
-import React, {memo} from "react";
+import React, {memo, useState} from "react";
 import SearchCard, {SearchCardType} from "../BaseComponents/SearchCard/SearchCard";
 
 // import styles
@@ -11,19 +11,22 @@ import {getSearchValue} from "../../stores/searchStore";
 import {useSelector} from "react-redux";
 import {useDebounce} from "../../lib/debounce";
 import {usePackageSearch} from "../../lib/usePackageSearch";
+import LoadingData from "../BaseComponents/LoadingData/LoadingData";
 
 
 
 export default memo(function SearchPackagesResult(): React.JSX.Element {
+  const [ isLoading, setIsLoading ] = useState<boolean>(false)
   const currentValue: string = useSelector(getSearchValue)
   const debouncedValue: string = useDebounce(currentValue, 500);
-  const packagesData: [SearchCardType] = usePackageSearch(debouncedValue)
+  const packagesData: [SearchCardType] = usePackageSearch(debouncedValue, setIsLoading)
   return (
       <div className={styles.searchPackagesResultBlock}>
-          {packagesData?.length > 0 && packagesData.map((card:SearchCardType , ind: number)=>{
+          {!isLoading && packagesData?.length > 0 && packagesData.map((card:SearchCardType , ind: number)=>{
               return <SearchCard key={ind} card={card}/>
           })}
-          {packagesData?.length === 0 && <p>No data</p>}
+          {!isLoading && packagesData?.length === 0 && <p>No data</p>}
+          {isLoading && <LoadingData/>}
       </div>
   );
 })
--- app/lib/usePackageSearch.tsx
+++ app/lib/usePackageSearch.tsx
@@ -4,19 +4,21 @@ import {SearchCardType} from "../components/BaseComponents/SearchCard/SearchCard
 const url = 'https://libraries.io/api/search'
 const githubKey = 'adea9f496220f5bf0a57a2fc91bfe40b'
 const searchCache = {};
-const getPackages = async (value: string) => {
+const getPackages = async (value: string, setIsLoading: (boolean)=>void) => {
+    setIsLoading(true)
     if (!searchCache[value]) {
       const response = await fetch(`${url}?q=${value}&api_key=${githubKey}`)
       const packagesData = await response.json();
       searchCache[value] = packagesData;
     }
+    setIsLoading(false)
     return searchCache[value];
 }
 
-export const usePackageSearch = (value: string): [SearchCardType] => {
+export const usePackageSearch = (value: string, setIsLoading: (boolean)=>void): [SearchCardType] => {
     const [ packages, setPackages ] = useState<[SearchCardType]>([])
     useEffect(()=> {
-     value.length && getPackages(value).then(setPackages)
+     value.length && getPackages(value, setIsLoading).then(setPackages)
      !value.length && setPackages([])
     }, [value]);
     return packages;
