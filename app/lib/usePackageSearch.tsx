3e04c83390e2c9153f22038fa0ee2123581ef7db
Tatsiana-Salikayeva <tatsiana.salikayeva@promaterial.com>
2024-03-05 19:08:39 +0100

    format usePackageSearch view

--- app/lib/usePackageSearch.tsx
+++ app/lib/usePackageSearch.tsx
@@ -8,60 +8,62 @@ const url = 'https://libraries.io/api/search'
 const githubKey = 'adea9f496220f5bf0a57a2fc91bfe40b'
 
 export const getPackages = async (value: string, sort: string,
-    page: number, setPageNumber: (T) => void, searchCache: Object, dispatch: (T) => void) => {
-    try {
-        const searchCacheLocal = {}
-        let response = null;
-        const mainUrl = `${url}?q=${value}`;
-        const sortParam = `&sort=${sort}`;
-        const pageParam = `&page=${page}&per_page=5`;
-        response = await fetch(`${mainUrl}${sortParam}${pageParam}&api_key=${githubKey}`)
-        if (response.status === 404) {
-            const newPageParam = `&page=1&per_page=5`;
-            response = await fetch(`${mainUrl}${sortParam}${newPageParam}&api_key=${githubKey}`)
-            setPageNumber(1)
-        }
-        const totalPages = Number(response?.headers?.get('total')) || 1;
-        dispatch(setTotalPagesFetched(totalPages));
-        const packagesData = await response.json();
-        searchCacheLocal[`${value}-${sort}-${page}`] = packagesData;
-        dispatch(setSearchCache(searchCacheLocal));
-        return searchCacheLocal;
-    } catch (error: Error) {
-        console.log(error);
-    }
+  page: number, setPageNumber: (T) => void, searchCache: Object,
+  dispatch: (T) => void) => {
+      try {
+          const searchCacheLocal = {}
+          let response = null;
+          const mainUrl = `${url}?q=${value}`;
+          const sortParam = `&sort=${sort}`;
+          const pageParam = `&page=${page}&per_page=5`;
+          response = await fetch(`${mainUrl}${sortParam}${pageParam}&api_key=${githubKey}`)
+          if (response.status === 404) {
+              const newPageParam = `&page=1&per_page=5`;
+              response = await fetch(`${mainUrl}${sortParam}${newPageParam}&api_key=${githubKey}`)
+              setPageNumber(1)
+          }
+          const totalPages = Number(response?.headers?.get('total')) || 1;
+          dispatch(setTotalPagesFetched(totalPages));
+          const packagesData = await response.json();
+          searchCacheLocal[`${value}-${sort}-${page}`] = packagesData;
+          dispatch(setSearchCache(searchCacheLocal));
+          return searchCacheLocal;
+      } catch (error: Error) {
+          console.log(error);
+      }
 }
 
-export const usePackageSearch = (value: string, setIsLoading: (boolean)=>void,
-    isSortedByStars: boolean, page: number, setPageNumber: (T) => void ): [SearchCardType] => {
-    const [packages, setPackages] = useState<[SearchCardType]>([]);
-    const [isResultLoaded, setIsResultLoaded ] = useState<boolean>(false);
-    const searchCache: Object = useSelector(getFetchCache);
-    const dispatch = useAppDispatch();
+export const usePackageSearch = (value: string, setIsLoading: (boolean) => void,
+  isSortedByStars: boolean, page: number,
+  setPageNumber: (T) => void): [SearchCardType] => {
+      const [packages, setPackages] = useState<[SearchCardType]>([]);
+      const [isResultLoaded, setIsResultLoaded] = useState<boolean>(false);
+      const searchCache: Object = useSelector(getFetchCache);
+      const dispatch = useAppDispatch();
 
-    useEffect(()=> {
-        setIsResultLoaded(false)
-        if (!value.length) return setPackages([])
-        const sort = isSortedByStars ? 'stars' : '';
-        const cacheKey = `${value}-${sort}-${page}`;
-        if (searchCache[cacheKey]) {
-            setPackages(searchCache[cacheKey] || []);
-            setIsLoading(false)
-            } else {
-                setIsLoading(true)
-                getPackages(value, sort, page, setPageNumber,
-                    searchCache, dispatch).then((responce) => {
-                    if (!responce) {
-                        setPackages([]);
-                        setIsLoading(false)
-                        return;
-                    }
+      useEffect(() => {
+          setIsResultLoaded(false)
+          if (!value.length) return setPackages([])
+          const sort = isSortedByStars ? 'stars' : '';
+          const cacheKey = `${value}-${sort}-${page}`;
+          if (searchCache[cacheKey]) {
+              setPackages(searchCache[cacheKey] || []);
+              setIsLoading(false)
+          } else {
+              setIsLoading(true)
+              getPackages(value, sort, page, setPageNumber,
+                  searchCache, dispatch).then((responce) => {
+                  if (!responce) {
+                      setPackages([]);
+                      setIsLoading(false)
+                      return;
+                  }
                   setIsResultLoaded(true)
-                });
-            }
-    }, [value, isResultLoaded, isSortedByStars, page]);
+              });
+          }
+      }, [value, isResultLoaded, isSortedByStars, page]);
 
-    useEffect(()=> {
+    useEffect(() => {
         setPageNumber(1)
     }, [value]);
 
